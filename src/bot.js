165643681ea95b8d120a7f410f2e1f62197e1175
TheUndercoverCEO <55495874+TheUndercoverCEO@users.noreply.github.com>
2021-04-02 18:36:32 +1100

    add meme voting

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,6 @@
 // Requirements
 require("dotenv").config();
-const { Client, MessageEmbed } = require("discord.js");
+const { Client, MessageEmbed, Message, Guild } = require("discord.js");
 
 // Create required variables
 const client = new Client({
@@ -11,14 +11,14 @@ const roleReactMsg = "827434586589102110";
 
 // Start Message
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log("Undercover bot is online @ " + client.readyAt + ".");
 });
   
 
 // Reaction Panel Send Command
 client.on('message', message => {
-    if (message.author.bot) return;
-    if (message.content.startsWith(prefix)){
+    if (message.author.bot) return; // If message was sent by a got ignore
+    if (message.content.startsWith(prefix)){ // If the message was sent with the prefix
         const [cmd, ...args] = message.content
             .trim()
             .substring(prefix.length)
@@ -34,7 +34,12 @@ client.on('message', message => {
                 .setDescription("<:YouTube:709605862985039872> : I am a YouTuber\n<:twitch_streamer:709581340382724246> : I am a Twitch Streamer\n:page_facing_up: : Notify me about: **Miscellaneous (including twitch channel updates, etc)**\n:circus_tent: : Notify me about: **Events (including stream events)**\n:camera_with_flash: : Notify me about: **YouTube Videos**")
                 .setFooter(`${client.user.username}`, "https://i.imgur.com/k6EqY8f.png");
             message.channel.send(embed);
+        } else if(true) {
+
         }
+    } else if(message.channel.id === "715689948711223366"){
+        message.react("⬆️");
+        message.react("⬇️");
     }
 
 });
@@ -59,6 +64,14 @@ client.on("messageReactionAdd", (reaction, user) => {
             case "📸":
                 member.roles.add("709887160936824852");
                 break;
+        }
+    } else if(reaction.message.channel.id === "715689948711223366"){
+        if(name === "⬆️" || name === "⬇️"){
+            const totalReaction = reaction.message.reactions.resolve("⬆️").count + reaction.message.reactions.resolve("⬆⬇️").count
+            if(totalReaction > 7 && reaction.message.reactions.resolve("⬆️").count / totalReaction > 0.8){
+                member.roles.add("827443852192907264", "posted a good meme");
+                client.channels.get("709607662806892594").send(reaction.message.author.username + " just became a meme god. GG!")
+            }
         }
     }
 }); 
