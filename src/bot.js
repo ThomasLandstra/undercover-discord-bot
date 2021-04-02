b05a27681e19ae2303cde8021261a046b71cfe03
TheUndercoverCEO <55495874+TheUndercoverCEO@users.noreply.github.com>
2021-04-03 10:46:43 +1100

    added internal documentation

--- src/bot.js
+++ src/bot.js
@@ -19,13 +19,16 @@ client.on('ready', () => {
 client.on('message', async message => {
     if (message.author.bot) return; // If message was sent by a got ignore
     if (message.content.startsWith(prefix)){ // If the message was sent with the prefix
-        const [cmd, ...args] = message.content
-            .trim()
-            .substring(prefix.length)
-            .split(/\s+/);
+        // Split Command
+        const [cmd, ...args] = message.content.trim().substring(prefix.length).split(/\s+/);
+
+        //Ping Command
         if(cmd === "ping"){
             message.channel.send("<@"+message.author.id+"> pong!");
-        } else if(cmd === "reactMsg" && message.author.id === "277186846117724160"){
+        }
+
+        // Send Role React Message Command
+        else if(cmd === "reactMsg" && message.author.id === "277186846117724160"){
             message.delete();
 
             const embed = new MessageEmbed()
@@ -34,7 +37,10 @@ client.on('message', async message => {
                 .setDescription("<:YouTube:709605862985039872> : I am a YouTuber\n<:twitch_streamer:709581340382724246> : I am a Twitch Streamer\n:page_facing_up: : Notify me about: **Miscellaneous (including twitch channel updates, etc)**\n:circus_tent: : Notify me about: **Events (including stream events)**\n:camera_with_flash: : Notify me about: **YouTube Videos**")
                 .setFooter(`${client.user.username}`, "https://i.imgur.com/k6EqY8f.png");
             message.channel.send(embed);
-        } else if(cmd === "help") {
+        }
+
+        // Help Command
+        else if(cmd === "help") {
             const embed = new MessageEmbed()
                 .setTitle("Undercover Bot Help Menu")
                 .setColor(2072139)
@@ -48,49 +54,55 @@ client.on('message', async message => {
 
 });
 
+// On Reaction Add
 client.on("messageReactionAdd", async (reaction, user) => {
     const { name } = reaction.emoji;
     const member = reaction.message.guild.members.cache.get(user.id);
+
+    // Role React
     if (reaction.message.id === roleReactMsg){
         switch (name) {
             case "YouTube":
-                member.roles.add("709529044072267906");
+                member.roles.add("709529044072267906"); // YouTuber Role
                 break;
             case "twitch_streamer":
-                member.roles.add("709528954305904650");
+                member.roles.add("709528954305904650"); // Twitch Streamer Role
                 break;
             case "📄":
-                member.roles.add("732529645206896680");
+                member.roles.add("732529645206896680"); // Announce Misc Role
                 break;
             case "🎪":
-                member.roles.add("732529543646019675");
+                member.roles.add("732529543646019675"); // Announce Events Role
                 break;
             case "📸":
-                member.roles.add("709887160936824852");
+                member.roles.add("709887160936824852"); // Anounce YouTube Videos
                 break;
         }
     }
 }); 
 
+// On reaction remove
 client.on("messageReactionRemove", async (reaction, user) => {
     const { name } = reaction.emoji;
     const member = reaction.message.guild.members.cache.get(user.id);
+
+    // Role React
     if (reaction.message.id === roleReactMsg){
         switch (name) {
             case "YouTube":
-                member.roles.remove("709529044072267906");
+                member.roles.remove("709529044072267906"); // YouTuber Role
                 break;
             case "twitch_streamer":
-                member.roles.remove("709528954305904650");
+                member.roles.remove("709528954305904650"); // Twitch Streamer Role
                 break;
             case "📄":
-                member.roles.remove("732529645206896680");
+                member.roles.remove("732529645206896680"); // Announce Misc Role
                 break;
             case "🎪":
-                member.roles.remove("732529543646019675");
+                member.roles.remove("732529543646019675"); // Announce Events Role
                 break;
             case "📸":
-                member.roles.remove("709887160936824852");
+                member.roles.remove("709887160936824852"); // Anounce YouTube Videos
                 break;
         }
     }
